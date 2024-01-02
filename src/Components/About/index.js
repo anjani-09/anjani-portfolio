d34ae6719874d8fba70011302b186f2074c425e6
Anjani Rajanala <anjanirajanala@Anjanis-MacBook-Air.local>
2024-01-02 10:05:35 -0700

    Deployed Website adjusted font size on About Page

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -37,10 +37,10 @@ const About = () => {
               idx={15}
             />
             </h1>
-            <p style={{ fontSize: '18px' }}>
+            <p style={{ fontSize: '16px' }}>
 My journey through <strong>Arizona State University</strong> culminated in the attainment of a Masters in Information Technology, a testament to my fervor for technology's transformative power. Through rigorous academic pursuits, I delved into diverse IT methodologies, cultivating a robust skill set and an unwavering passion for innovation.
             </p>
-            <p style={{ fontSize: '18px' }}>
+            <p style={{ fontSize: '16px' }}>
             My tenure at <strong>Amazon</strong> as a <strong>Software Development Engineer</strong> was a defining chapter. I spearheaded a gripping Full Stack project, immersing myself in the intricate landscape of React. This experience fueled my evolution as I seamlessly maneuvered between frontend intricacies and backend complexities. <strong>Graduating in December of 2023</strong> marked a pivotal moment, propelling me into a world of fresh opportunities. I now embark on a quest for new graduate developer roles, driven by an insatiable curiosity and an unyielding commitment to contribute meaningfully to groundbreaking tech endeavors. My aspiration is to merge academic insights with practical finesse, crafting solutions that resonate within the dynamic tech sphere.
             </p>
           </div>
