a744c5d5bc4300d0cf6170266a8c0c984b79ae92
Anjani Rajanala <anjanirajanala@Anjanis-MacBook-Air.local>
2024-01-22 19:21:36 -0700

    mobile version nav bar fixing

--- src/Components/SideBar/index.js
+++ src/Components/SideBar/index.js
@@ -18,7 +18,7 @@ import {
 import { Link, NavLink } from 'react-router-dom'
 
 const Sidebar = () => {
-  const [showNav, setShowNav] = useState(true);
+  const [showNav, setShowNav] = useState(false);
 
   return (
     <div className="nav-bar">
